33f6c22e704134e91e7145d17c4157fe54f99c73
Nadezhda <nadezhdadieslast@gmail.com>
2024-04-12 22:36:14 +0400

    made the matrix diagonal

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 //comparing matrices
 
-void fillArray(int arr[4][4], int value) {
+void fillArray(int arr[4][4]) {
+    int symbol;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            std::cin >> value;
-            arr[i][j] = value;
+            std::cin >> symbol;
+            arr[i][j] = symbol;
         }
     }
 }
@@ -32,19 +33,30 @@ bool compareTwoMatrices(int arr[4][4], int arr2[4][4]) {
     return true;
 }
 
+void diagonalMatrix(int arr[4][4]){
+    arr[0][1] = 0;
+    arr[0][2] = 0;
+    arr[0][3] = 0;
+    arr[1][2] = 0;
+    arr[1][3] = 0;
+    arr[2][3] = 0;
+}
+
 int main() {
 
     int arr[4][4];
     int arr2[4][4];
-    int value = 0;
 
-    fillArray(arr, value);
-    fillArray(arr2, value);
+    std::cout << "Input elements in matrix: ";
+    fillArray(arr);
+    std::cout << "Input elements in matrix: ";
+    fillArray(arr2);
     if (compareTwoMatrices(arr, arr2)) {
-        std::cout << "Matrices are similar";
+        std::cout << "Matrices are similar" << std::endl;
     } else {
-        std::cout << "Matrices aren't similar";
+        std::cout << "Matrices aren't similar" << std::endl;
         return 0;
     }
-
+    diagonalMatrix(arr);
+    printArray(arr);
 }
